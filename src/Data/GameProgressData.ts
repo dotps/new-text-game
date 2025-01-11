0fce8de1222e24286dd8d05197fa66c7d6408ac8
Stas Petrov <petrovstas@mail.ru>
2025-01-11 11:59:57 +0500

    feat: add new scenario

--- src/Data/GameProgressData.ts
+++ src/Data/GameProgressData.ts
@@ -1,6 +1,4 @@
 export class GameProgressData {
     public currentLocationId: string = "start"
-    public currentLevelPath: string = "./Data/level.json"
-    // public currentLocation: ILocation = {id: this.currentLocationId, actions: [], description: "", title: ""}
-    // public currentLocation: ILocation = {id: "start", actions: [], description: "", title: ""}
+    public currentLevelPath: string = "./Data/level-2.json"
 }
